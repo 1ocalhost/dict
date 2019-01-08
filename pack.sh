7fe88603c6be2024a00a03e348429854d255ff2f
Pamela <aomeidev@gmail.com>
2019-01-08 23:44:04 +0800

    Update pack.sh

--- pack.sh
+++ pack.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 out="pack.zip"
 self=`basename "$0"`
+exclude='.git/\* \*_d.js .gitignore README.md'
 gitignore=$(awk '!/^$/{if ($0 ~ /\/$/) {x="\\*"} else {x=""} printf $0""x" "}' .gitignore)
-eval "rm -f $out && zip -r $out . -x $self .git/\\* $gitignore"
+eval "rm -f $out && zip -r $out . -x $self $exclude $gitignore"
