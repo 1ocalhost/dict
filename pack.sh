a0dd611f63574f2cc5f3eab4d6210487c9937691
Pamela <aomeidev@gmail.com>
2019-01-12 00:47:10 +0800

    Add screenshot

--- pack.sh
+++ pack.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 out="pack.zip"
 self=`basename "$0"`
-exclude='.git/\* \*_d.js .gitignore README.md'
+exclude='.git/\* \*_d.js .gitignore README.md store_assets/\*'
 gitignore=$(awk '!/^$/{if ($0 ~ /\/$/) {x="\\*"} else {x=""} printf $0""x" "}' .gitignore)
 eval "rm -f $out && zip -r $out . -x $self $exclude $gitignore"
