1f1de5e744322b9706712f5de45ed8bc604b0964
Pamela <aomeidev@gmail.com>
2019-06-30 15:03:15 +0800

    Update pack.sh

--- pack.sh
+++ pack.sh
@@ -1,4 +1,18 @@
 #!/bin/bash
+cd "$(dirname "${BASH_SOURCE[0]}")"
+read_version="import sys, json; print(json.load(sys.stdin)['version'])"
+ver_value=$(cat manifest.json | python3 -c "$read_version")
+ver_date=$(date +.%y%m%d.)
+
+if [[ $ver_value != *$ver_date* ]]; then
+    echo "[$ver_value] is not contains: $ver_date"
+    read -p "Continue? (y/n)" -n 1 -r
+    echo
+    if [[ ! $REPLY =~ ^[Yy]$ ]]; then
+        [[ "$0" = "$BASH_SOURCE" ]] && exit 1 || return 1
+    fi
+fi
+
 out="pack.zip"
 self=`basename "$0"`
 exclude='.git/\* \*_d.js .gitignore README.md store_assets/\*'
